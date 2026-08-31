0870b86a9a96fd2730a99dbbf537fe63c5ebc986
kiwi <khushipatro06@gmail.com>
2026-08-31 23:07:04 +0530

    Add Student class with properties and methods

---
+++ model/Student.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Student {
+
+    private int studentId;
+    private String name;
+    private String email;
+    private String password;
+
+    public Student() {
+    }
+
+    public Student(int studentId, String name, String email, String password) {
+        this.studentId = studentId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
