5659ff27f23d35e7dac002da852f99cd46d51db1
kiwi <khushipatro06@gmail.com>
2026-09-01 00:24:36 +0530

    Refactor StudentDAOInterface to include login method
    Removed methods for adding and retrieving all students, added login method.

--- model/dao/StudentDAOInterface.java
+++ model/dao/StudentDAOInterface.java
@@ -1,13 +1,10 @@
 package dao;
 
 import model.Student;
-import java.util.List;
 
-public interface StudentDAOInterface { 
-
-    void addStudent(Student student);
+public interface StudentDAOInterface {
 
     Student getStudentById(int studentId);
 
-    List<Student> getAllStudents();
+    Student login(String email, String password);
 }
